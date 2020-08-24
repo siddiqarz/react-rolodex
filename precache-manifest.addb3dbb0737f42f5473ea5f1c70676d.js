f75d4ef7425647be2ff23f55dc4c9faee61a9f4d
Siddiqa R. Z <siddiqarasheed@gmail.com>
2020-08-23 23:07:54 -0400

    Updates

---
+++ precache-manifest.addb3dbb0737f42f5473ea5f1c70676d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6919a1e27d31111f6bf4edef02a5b57",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "d828eb03c32b6cc53840",
+    "url": "/react-rolodex/static/css/main.1b016f40.chunk.css"
+  },
+  {
+    "revision": "f671a9a35403e13b683f",
+    "url": "/react-rolodex/static/js/2.8e27886e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.8e27886e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d828eb03c32b6cc53840",
+    "url": "/react-rolodex/static/js/main.3d6b5c49.chunk.js"
+  },
+  {
+    "revision": "c4d1c4685314a12a1a2e",
+    "url": "/react-rolodex/static/js/runtime-main.01269c3e.js"
+  }
+]);
